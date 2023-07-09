9fba9df292f39d615c489ff582760c1aa9477f02
niyasrad <niyasrad@gmail.com>
2023-07-09 11:23:06 +0530

    feat: employee signup, signin and util management

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const adminRoute = require('./admin.routes')
+const employeeRoute = require('./employee.routes')
 
 const combineRoutes = (app) => {
     app.use('/api/admin', adminRoute)
+    app.use('/api/employee', employeeRoute)
 }
 
 module.exports = combineRoutes
