55dbc8cb867ab11e358ebaac76659766d0866703
Niyas Hameed <niyasrad@gmail.com>
2023-07-12 15:13:54 +0530

    feat(auth): middleware, checkauth route - implemented (#5)

--- src/utils/jwt.util.js
+++ src/utils/jwt.util.js
@@ -4,4 +4,27 @@ const generateToken = (payload) => {
     return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '2d' })
 }
 
-module.exports = { generateToken }
+const verifyToken = (req, res, next) => {
+    
+    try {
+
+        const token = req.headers['authorization'].split(' ')[1]
+
+        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+            if (err || !decoded) return res.status(403).json({
+                message: "Login has failed!"
+            })
+            req.username = decoded.username; 
+            next()
+        })
+
+    } catch (err) {
+
+        return res.status(403).json({
+            message: "You are not authorized to access this resource"
+        })
+
+    }
+}
+
+module.exports = { generateToken, verifyToken }
